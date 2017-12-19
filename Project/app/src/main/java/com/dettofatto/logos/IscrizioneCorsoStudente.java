ae6c3727c0e3625a27d3f16f0914f435a170d68d
1ITS-master\itsadmin <mattiabonzi@libero.it>
2017-12-19 17:24:57 +0100

    Layout IscrivitiCorsoStudente

---
+++ Project/app/src/main/java/com/dettofatto/logos/IscrizioneCorsoStudente.java
@@ -0,0 +1,13 @@
+package com.dettofatto.logos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IscrizioneCorsoStudente extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iscrizione_corso_studente);
+    }
+}
