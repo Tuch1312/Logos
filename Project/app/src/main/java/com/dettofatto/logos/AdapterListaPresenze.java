c421ddd0868d377aac7dc2842abd7de909085006
Giovaaa <g.giovannelli@itsrizzoli.it>
2018-02-07 16:57:16 +0100

    PRESENZEASSENZE layout (errori nella parte java)

--- Project/app/src/main/java/com/dettofatto/logos/AdapterListaPresenze.java
+++ Project/app/src/main/java/com/dettofatto/logos/AdapterListaPresenze.java
@@ -9,8 +9,6 @@ import android.view.ViewGroup;
 import android.widget.Switch;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 
 
@@ -18,12 +16,12 @@ import java.util.ArrayList;
  * Created by itsadmin on 22/01/2018.
  */
 
-public class Adapter extends ArrayAdapter<presenzarow> {
+public class AdapterListaPresenze extends ArrayAdapter<Presenzarow> {
 
     Context ctx;  // superclasse da cui si estedono sottoclassi, tra ci c'è la mia activity
-    ArrayList<presenzarow> values;
+    ArrayList<Presenzarow> values;
 
-    public Adapter(Context ctx, ArrayList<presenzarow> values) {
+    public AdapterListaPresenze(Context ctx, ArrayList<Presenzarow> values) {
         super(ctx, R.layout.rows_list_presenze, values);
 
         this.ctx = ctx;
@@ -34,11 +32,11 @@ public class Adapter extends ArrayAdapter<presenzarow> {
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View rowView = inflater.inflate(R.layout.rows_list_presenze, parent, false);
-        TextView nomeView = (TextView) rowView.findViewById(R.id.nome);
-        TextView cognomeView = (TextView) rowView.findViewById(R.id.cognome);
-        TextView ingressoView = (TextView) rowView.findViewById(R.id.oraingresso);
-        TextView uscitaView = (TextView) rowView.findViewById(R.id.orauscita);
-        Switch swi = (Switch) rowView.findViewById(R.id.switchbtn);
+        TextView nomeView = (TextView) rowView.findViewById(R.id.nomeStudPres);
+        TextView cognomeView = (TextView) rowView.findViewById(R.id.cognomeStudPres);
+        TextView ingressoView = (TextView) rowView.findViewById(R.id.ingressoPres);
+        TextView uscitaView = (TextView) rowView.findViewById(R.id.uscitaPres);
+        Switch swi = (Switch) rowView.findViewById(R.id.studentePresAss);
 
 
         nomeView.setText(values.get(position).nome);
--- Project/app/src/main/java/com/dettofatto/logos/presenzarow.java
+++ Project/app/src/main/java/com/dettofatto/logos/presenzarow.java
@@ -6,16 +6,56 @@ import java.util.Date;
  * Created by itsadmin on 30/01/2018.
  */
 
-public class presenzarow {
-    public String nome, cognome;
-    public Date ingresso, uscita;
-    public boolean presass;
+public class Presenzarow {
+    private String nome, cognome;
+    private Date ingresso, uscita;
+    private boolean presass;
 
-    public presenzarow(String n, String c, Date i, Date u) {
+    public Presenzarow(String n, String c, Date i, Date u) {
         nome = n;
         cognome = c;
         ingresso = i;
         uscita = u;
         presass = false;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public Date getIngresso() {
+        return ingresso;
+    }
+
+    public void setIngresso(Date ingresso) {
+        this.ingresso = ingresso;
+    }
+
+    public Date getUscita() {
+        return uscita;
+    }
+
+    public void setUscita(Date uscita) {
+        this.uscita = uscita;
+    }
+
+    public boolean isPresass() {
+        return presass;
+    }
+
+    public void setPresass(boolean presass) {
+        this.presass = presass;
     }
 }
