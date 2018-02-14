07e63142c00e3ba88e3d640693f8df8b46cca821
Giovaaa <g.giovannelli@itsrizzoli.it>
2018-02-14 10:38:58 +0100

    android retrofit (inizio)

--- Project/app/src/main/java/com/dettofatto/logos/Login.java
+++ Project/app/src/main/java/com/dettofatto/logos/Login.java
@@ -3,6 +3,9 @@ package com.dettofatto.logos;
 import android.app.Activity;
 import android.os.Bundle;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class Login extends Activity {
 
     @Override
@@ -10,4 +13,5 @@ public class Login extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
 }
