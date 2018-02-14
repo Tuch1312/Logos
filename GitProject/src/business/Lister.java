4541a36d369eaed1e69ea2c8f04556f072cac341
Meringone <f.benassi@itsrizzoli.it>
2018-02-14 10:17:56 +0100

    101%

--- GitProject/src/business/Lister.java
+++ GitProject/src/business/Lister.java
@@ -16,6 +16,7 @@ import javax.persistence.EntityManager;
 
 import entity.Corso;
 import entity.Docente;
+import entity.Iscrizione;
 import entity.Lezione;
 import entity.Studente;
 
@@ -30,8 +31,14 @@ public class Lister {
 	 */
 	public List<Lezione> getLezioniPerCorso(Corso c) {
 		EntityManager em = JPAUtility.emf.createEntityManager();
+		Corso corso = null;
+		try {
+			corso = em.find(Corso.class, c.getIdCorso());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		List<Lezione> lezioni = em.createQuery("select l from Lezione l where l.corso = :corso", Lezione.class)
-				.setParameter("corso", c).getResultList();
+				.setParameter("corso", corso).getResultList();
 		return lezioni;
 	}
 
@@ -42,8 +49,14 @@ public class Lister {
 	 */
 	public List<Corso> getCorsiPerDocente(Docente d) {
 		EntityManager em = JPAUtility.emf.createEntityManager();
+		Docente docente = null;
+		try {
+			docente = em.find(Docente.class, d.getMail());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		List<Corso> corsi = em.createQuery("select c from Corso c where c.docente = :docente", Corso.class)
-				.setParameter("docente", d).getResultList();
+				.setParameter("docente", docente).getResultList();
 		return corsi;
 
 	}
@@ -55,10 +68,16 @@ public class Lister {
 	 */
 	public List<Corso> getCorsiPerStudente(Studente s) {
 		EntityManager em = JPAUtility.emf.createEntityManager();
+		Studente studente = null;
+		try {
+			studente = em.find(Studente.class, s.getMail());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		List<Corso> corsi = em
 				.createQuery("select c from Iscrizione i join i.corso c where i.studenteIscritto = :studente",
 						Corso.class)
-				.setParameter("studente", s).getResultList();
+				.setParameter("studente", studente).getResultList();
 		return corsi;
 
 	}
@@ -69,10 +88,16 @@ public class Lister {
 	 */
 	public List<Studente> getStudentiPerCorso(Corso c) {
 		EntityManager em = JPAUtility.emf.createEntityManager();
+		Corso corso = null;
+		try {
+			corso = em.find(Corso.class, c.getIdCorso());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		List<Studente> studenti = em
 				.createQuery("select s from Iscrizione i join i.studenteIscritto s where i.corso = :corso",
 						Studente.class)
-				.setParameter("corso", c).getResultList();
+				.setParameter("corso", corso).getResultList();
 		return studenti;
 
 	}
@@ -84,9 +109,15 @@ public class Lister {
 
 	public List<Lezione> getLezioniDiOggi(Docente d) {
 		EntityManager em = JPAUtility.emf.createEntityManager();
+		Docente docente = null;
+		try {
+			docente = em.find(Docente.class, d.getMail());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		List<Lezione> lezioni = em.createQuery(
 				"select l from Lezione l where l.data = :data and l.corso = (select c from Corso c where c.docente = :docente)",
-				Lezione.class).setParameter("data", new Date()).setParameter("docente", d).getResultList();
+				Lezione.class).setParameter("data", new Date()).setParameter("docente", docente).getResultList();
 		return lezioni;
 	}
 
@@ -96,23 +127,36 @@ public class Lister {
 	 */
 	public List<Lezione> getLezioniDiDomani(Docente d) {
 		EntityManager em = JPAUtility.emf.createEntityManager();
+		Docente docente = null;
+		try {
+			docente = em.find(Docente.class, d.getMail());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(new Date());
 		cal.add(Calendar.DAY_OF_MONTH, 1);
 		List<Lezione> lezioni = em.createQuery(
 				"select l from Lezione l where l.data = :data and l.corso = (select c from Corso c where c.docente = :docente)",
-				Lezione.class).setParameter("data", cal.getTime()).setParameter("docente", d).getResultList();
+				Lezione.class).setParameter("data", cal.getTime()).setParameter("docente", docente).getResultList();
 		return lezioni;
 	}
 
 	public List<Lezione> getLezioniDiOggi(Studente s) {
+		EntityManager em = JPAUtility.emf.createEntityManager();
+		Studente studente = null;
+		try {
+			studente = em.find(Studente.class, s.getMail());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		String data = sdf.format(new Date());
 		List<Lezione> elenco = new ArrayList<Lezione>();
 		try {
 			Connection con = getConnection();
 			Statement query = con.createStatement();
-			String sql1 = "select `IDCORSO` from `ISCRIZIONE` where `MAILSTUDENTE` = \"" + s.getMail() + "\"";
+			String sql1 = "select `IDCORSO` from `ISCRIZIONE` where `MAILSTUDENTE` = \"" + studente.getMail() + "\"";
 			ResultSet ids = query.executeQuery(sql1);
 			List<Integer> listaId = new ArrayList<Integer>();
 			while (ids.next()) {
@@ -139,13 +183,20 @@ public class Lister {
 	}
 	
 	public List<Lezione> getLezioniDiDomani(Studente s) {
+		EntityManager em = JPAUtility.emf.createEntityManager();
+		Studente studente = null;
+		try {
+			studente = em.find(Studente.class, s.getMail());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		String data = sdf.format(new Date(new Date().getTime() + 86400000L));
 		List<Lezione> elenco = new ArrayList<Lezione>();
 		try {
 			Connection con = getConnection();
 			Statement query = con.createStatement();
-			String sql1 = "select `IDCORSO` from `ISCRIZIONE` where `MAILSTUDENTE` = \"" + s.getMail() + "\"";
+			String sql1 = "select `IDCORSO` from `ISCRIZIONE` where `MAILSTUDENTE` = \"" + studente.getMail() + "\"";
 			ResultSet ids = query.executeQuery(sql1);
 			List<Integer> listaId = new ArrayList<Integer>();
 			while (ids.next()) {
@@ -170,6 +221,20 @@ public class Lister {
 		}
 		return elenco;
 	}
+	
+	/*
+     * Ritorna tutte le iscirzioni per un determinato corso
+     */
+    public List<Iscrizione> getIscrizioniperCorso(Corso c) {
+        EntityManager em = JPAUtility.emf.createEntityManager();
+        List<Iscrizione> iscirzioni = em
+                .createQuery("select i from Iscrizione i join i.studenteIscritto s where i.corso = :corso",
+                        Iscrizione.class)
+                .setParameter("corso", c).getResultList();
+        return iscirzioni;
+        
+    }
+
 
 	private Lezione assegnavalori(ResultSet rs) throws SQLException {
 		EntityManager em = JPAUtility.emf.createEntityManager();
