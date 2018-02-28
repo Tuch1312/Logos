a3a944b043833cb29bcfa12ffc724832888e1f28
Giovaaa <g.giovannelli@itsrizzoli.it>
2018-02-28 16:25:41 +0100

    drawable layout menu android

--- Project/app/src/main/java/com/dettofatto/logos/fragment/Fragment_dashboard_docente_lista_lezioni.java
+++ Project/app/src/main/java/com/dettofatto/logos/fragment/Fragment_dashboard_docente_lista_lezioni.java
@@ -1,10 +1,14 @@
 package com.dettofatto.logos.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.*;
+import android.widget.ListView;
 
+import com.dettofatto.logos.PresenzeAssenze;
 import com.dettofatto.logos.R;
+import com.dettofatto.logos.adapter.AdapterListaPresenze;
 
 
 public class Fragment_dashboard_docente_lista_lezioni extends Fragment {
@@ -16,6 +20,12 @@ public class Fragment_dashboard_docente_lista_lezioni extends Fragment {
         View V = inflater.inflate(R.layout.fragment_dashboard_docente_lista_lezioni, container, false);
 
         return V;
+    }
+
+    public void onViewCreated(View view){
+        final ListView lv = view.findViewById(R.id.listaPresAss);
+        Intent listapresass = new Intent(getActivity(), PresenzeAssenze.class);
+        startActivity(listapresass);
     }
 
 }
