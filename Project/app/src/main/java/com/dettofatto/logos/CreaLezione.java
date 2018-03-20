b15b7ac82be5d4087728c5dde63b1fe0e8fd1115
Giovaaa <g.giovannelli@itsrizzoli.it>
2018-03-20 14:38:34 +0100

    crea lezione activity e layout

--- Project/app/src/main/java/com/dettofatto/logos/CreaLezione.java
+++ Project/app/src/main/java/com/dettofatto/logos/CreaLezione.java
@@ -2,6 +2,11 @@ package com.dettofatto.logos;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.dettofatto.logos.entities.Lezione;
 
 public class CreaLezione extends Activity {
 
@@ -9,5 +14,27 @@ public class CreaLezione extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crea_lezione);
+
+        Button b = findViewById(R.id.btnCreaLezione);
+
+        final EditText editArgomenti = findViewById(R.id.editTextArgomenti);
+        final EditText editAula = findViewById(R.id.editTextAula);
+        final EditText editData = findViewById(R.id.editTextData);
+        final EditText editDurata = findViewById(R.id.editTextDurata);
+        final EditText editOraInizio = findViewById(R.id.editTextOraInizio);
+
+        final Lezione lezione = new  Lezione();
+
+        b.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                lezione.setArgomenti(editArgomenti.getText().toString());
+                lezione.setAula(editAula.getText().toString());
+                lezione.setData(Long.parseLong(editData.getText().toString()));
+                lezione.setDurata(Integer.parseInt(editDurata.getText().toString()));
+                lezione.setOraInizio(editOraInizio.getText().toString());
+            }
+        });
+
     }
 }
